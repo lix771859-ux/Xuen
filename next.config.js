a4fcf3110559355cbecd11656f024d28beffa28b
lix771859-ux <lix771859@gmail.com>
2026-01-22 13:25:22 -0800

    fix

--- next.config.js
+++ next.config.js
@@ -16,6 +16,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; frame-src https://js.stripe.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
